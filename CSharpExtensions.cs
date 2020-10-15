326e8e049d65f2475866237205c4470c11931242
HarisTheProgrammer <38538630+HarisTheProgrammer@users.noreply.github.com>
2020-10-15 12:43:56 +0100

    Add files via upload

---
+++ CSharpExtensions.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CodeAnalyzer
+{
+    public static class CSharpExtensions
+    {
+        public static ITypeSymbol GetTypeSymbol(
+           this SemanticModel semanticModel,
+           ExpressionSyntax expression,
+           CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return semanticModel
+                .GetTypeInfo(expression, cancellationToken)
+                .Type;
+        }
+
+        public static ISymbol GetSymbol(
+          this SemanticModel semanticModel,
+          ExpressionSyntax expression,
+          CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return semanticModel
+                .GetSymbolInfo(expression, cancellationToken)
+                .Symbol;
+        }
+
+        public static ISymbol GetSymbol(
+            this SemanticModel semanticModel,
+            AttributeSyntax attribute,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return semanticModel
+                .GetSymbolInfo(attribute, cancellationToken)
+                .Symbol;
+        }
+
+        public static IMethodSymbol GetExtensionMethodSymbol(
+           this SemanticModel semanticModel,
+           ExpressionSyntax expression)
+        {
+            if (GetSymbol(semanticModel, expression) is IMethodSymbol methodSymbol
+                && methodSymbol.IsExtensionMethod)
+            {
+                IMethodSymbol reducedFrom = methodSymbol.ReducedFrom;
+
+                if (reducedFrom != null)
+                    return reducedFrom;
+
+                return methodSymbol;
+            }
+
+            return null;
+        }
+
+        public static bool TryGetNameParts(this ExpressionSyntax expression, out IList<string> parts)
+        {
+            var partsList = new List<string>();
+            if (!TryGetNameParts(expression, partsList))
+            {
+                parts = null;
+                return false;
+            }
+
+            parts = partsList;
+            return true;
+        }
+
+        public static bool TryGetNameParts(this ExpressionSyntax expression, List<string> parts)
+        {
+            if (expression.IsKind(SyntaxKind.SimpleMemberAccessExpression))
+            {
+                var memberAccess = (MemberAccessExpressionSyntax)expression;
+                if (!TryGetNameParts(memberAccess.Expression, parts))
+                {
+                    return false;
+                }
+
+                return AddSimpleName(memberAccess.Name, parts);
+            }
+            else if (expression.IsKind(SyntaxKind.QualifiedName))
+            {
+                var qualifiedName = (QualifiedNameSyntax)expression;
+                if (!TryGetNameParts(qualifiedName.Left, parts))
+                {
+                    return false;
+                }
+
+                return AddSimpleName(qualifiedName.Right, parts);
+            }
+            else if (expression is SimpleNameSyntax simpleName)
+            {
+                return AddSimpleName(simpleName, parts);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static bool AddSimpleName(SimpleNameSyntax simpleName, List<string> parts)
+        {
+            if (!simpleName.IsKind(SyntaxKind.IdentifierName))
+            {
+                return false;
+            }
+
+            parts.Add(simpleName.Identifier.ValueText);
+            return true;
+        }
+    }
+}
---
+++ SymbolUtility.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Immutable;
+using CodeAnalyzer.Entities.Helpers;
+using Microsoft.CodeAnalysis;
+
+namespace CodeAnalyzer.Utilities
+{
+    public static class SymbolUtility
+    {
+        public static bool IsLinqIEnumerableWithPredicate(
+            IMethodSymbol methodSymbol,
+            string name,
+            int parameterCount)
+        {
+            if (methodSymbol.DeclaredAccessibility != Accessibility.Public)
+                return false;
+
+            if (methodSymbol.Name is null || !methodSymbol.Name.Equals(name))
+                return false;
+
+            INamedTypeSymbol containingType = methodSymbol.ContainingType;
+
+            if (containingType == null)
+                return false;
+
+            if (containingType.Equals(MetaDataNames.System_Linq_Enumerable))
+            {
+                ImmutableArray<IParameterSymbol> parameters = methodSymbol.Parameters;
+
+                return parameters.Length == parameterCount
+                    && parameters[0].Type.OriginalDefinition.SpecialType == SpecialType.System_Collections_Generic_IEnumerable_T
+                    && IsPredicateFunc(parameters[1].Type, methodSymbol.TypeArguments[0]);
+            }
+
+            return false;
+        }
+
+        public static bool IsLinqIEnumerableWithPredicate(IMethodSymbol methodSymbol, string name)
+        {
+            if (methodSymbol.DeclaredAccessibility != Accessibility.Public)
+                return false;
+
+            if (methodSymbol.Name is null || !methodSymbol.Name.Equals(name))
+                return false;
+
+            INamedTypeSymbol containingType = methodSymbol.ContainingType;
+
+            if (containingType == null)
+                return false;
+
+            if (MetaDataNames.System_Linq_Enumerable.Equals(containingType))
+            {
+                ImmutableArray<IParameterSymbol> parameters = methodSymbol.Parameters;
+
+                if (parameters.Length > 1)
+                {
+                    return parameters[0].Type.OriginalDefinition.SpecialType == SpecialType.System_Collections_Generic_IEnumerable_T
+                        && IsPredicateFunc(parameters[1].Type, methodSymbol.TypeArguments[0]);
+                }
+            }
+
+            return false;
+        }
+
+        public static bool IsPredicateFunc(ISymbol symbol, ITypeSymbol parameter)
+        {
+            if (!MetaDataNames.System_Func_T2.Equals(symbol))
+                return false;
+
+            ImmutableArray<ITypeSymbol> typeArguments = ((INamedTypeSymbol)symbol).TypeArguments;
+
+            return typeArguments.Length == 2
+                && typeArguments[0].Equals(parameter)
+                && typeArguments[1].SpecialType == SpecialType.System_Boolean;
+        }
+    }
+}
